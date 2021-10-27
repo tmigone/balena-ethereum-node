a662e4db218bba3de5917d59c2cfb3f60e0ea225
Tomás Migone <tomasmigone@gmail.com>
2021-10-27 00:22:58 -0300

    SSD initialization script
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # References:
 # - https://askubuntu.com/questions/162434/how-do-i-find-out-usb-speed-from-a-terminal
 # - https://askubuntu.com/questions/1103569/how-do-i-change-the-label-reported-by-lsblk
@@ -6,10 +7,53 @@
 
 
 FILENAME=${1:-"/blockdevices.json"}
-rm -f "$FILENAME"
+ETH_NODE_LABEL=${2:-"ethnode"}
+ETH_NODE_MOUNTPOINT=${3:-"/mnt/ethereum"}
 
+rm -f "$FILENAME"
+mkdir -p "$ETH_NODE_MOUNTPOINT"
+
+echo "Fetching block device's data..."
 get_block_devices "$FILENAME"
 eth_validate_devices "$FILENAME"
+echo "Devices found: $(print_block_devices $FILENAME)"
+
+echo "Checking devices for an initialized node..."
+ETH_NODE_DEVICE=$(eth_find_initialized_node "$FILENAME" "$ETH_NODE_LABEL")
+
+if [[ -n "$ETH_NODE_DEVICE" ]]; then
+  echo "Found initialized ethereum node at $ETH_NODE_DEVICE"
+  # Do ?
+else
+  echo "Could not find an initialized node! Looking for a suitable block device to initialize..."
+  CANDIDATE_DEVICES=($(eth_get_candidate_devices "$FILENAME"))
+  echo "Candidate devices: ${CANDIDATE_DEVICES[@]}"
+  for DEVICE in "${CANDIDATE_DEVICES[@]}"; do
+    # Initialize
+    echo "Initializing $DEVICE..."
+    format_block_device "$DEVICE" "$ETH_NODE_LABEL"
+
+    # Validate
+    get_block_devices "$FILENAME"
+    eth_validate_devices "$FILENAME"
+    ETH_NODE_DEVICE=$(eth_find_initialized_node "$FILENAME" "$ETH_NODE_LABEL")
+    if [[ -n "$ETH_NODE_DEVICE" ]]; then
+      break
+    fi
+  done
+fi
+
+mount "$ETH_NODE_DEVICE" "$ETH_NODE_MOUNT_POINT"
+
+function eth_find_initialized_node () {
+  local FILENAME=${1:-"/blockdevices.json"}
+  local NODE_LABEL=${2:-"ethnode"}
+
+  if [[ -f "$FILENAME" ]]; then
+    cat "$FILENAME" |
+      jq -r ".blockdevices[] | select(.children[]?.label==\"$NODE_LABEL\") | .children[] .path"
+  fi
+}
 
 # eth_validate_devices
 # Validates block devices testing for capabilities as storage medium for an ethereum node
@@ -20,7 +64,7 @@ eth_validate_devices "$FILENAME"
 # Usage: get_block_devices_info $filename $min_disk_size
 # Parameters:
 # - $filename: path to file where results are stored. Default: /blockdevices.json
-# - $min_disk_size: minimum disk size required. Default: 350 GB (required for ethereum mainnet as of Oct/2021)
+# - $min_disk_size: minimum disk size required (in GB). Default: 350 GB (required for ethereum mainnet as of Oct/2021)
 function eth_validate_devices () {
   local FILENAME=${1:-"/blockdevices.json"}
   local MIN_DISK_SIZE=${2:-350}
@@ -62,7 +106,7 @@ function eth_get_candidate_devices () {
 
   if [[ -f "$FILENAME" ]]; then
     cat "$FILENAME" |
-      jq '[.blockdevices[] | select(.ethmeta.usb==false and .ethmeta.usb3==false and .ethmeta.size==false)][0]'
+      jq -r '.blockdevices[] | select(.ethmeta.usb==true and .ethmeta.usb3==true and .ethmeta.size==true) | .path'
   fi
 }
 
@@ -74,11 +118,38 @@ function eth_get_candidate_devices () {
 function get_block_devices () {
   local FILENAME=${1:-"/blockdevices.json"}
 
-  lsblk --bytes --json --output=name,size,label,path,tran |                           # Get the data from lsblk
-    jq 'del(.blockdevices[] .children)' |                                             # Remove block children[]
-    jq '.blockdevices[] .size /= 1024*1024*1024' |                                    # Convert block size to GB
-    jq '.blockdevices[] .size |= floor' |                                             # Round block size to integer
-    tee "$FILENAME" > /dev/null                                                       # Save to file
+  lsblk --bytes --json --output=name,size,label,path,tran |         # Get the data from lsblk
+    jq '.blockdevices[] .size /= 1024*1024*1024' |                  # Convert block size to GB
+    jq '.blockdevices[] .size |= ceil' |                           # Round block size to integer
+    jq '.blockdevices[] .children[]? .size /= 1024*1024*1024' |     # Convert child block size to GB
+    jq '.blockdevices[] .children[]? .size |= ceil' |              # Round child block size to integer
+    tee "$FILENAME" > /dev/null                                     # Save to file
 }
 
+
+function print_block_devices () {
+  local FILENAME=${1:-"/blockdevices.json"}
+
+  if [[ -f "$FILENAME" ]]; then
+    cat "$FILENAME" |
+      jq -r '[.blockdevices[] .path] | join(" ")'
+  fi
+}
+
+function format_block_device () {
+  local DEVICE="$1"
+  local NODE_LABEL=${2:-"ethnode"}
+
+  if [[ -n "$DEVICE" ]]; then
+    echo "Formatting $DEVICE..."
+    parted "$DEVICE" --script mklabel msdos mkpart primary ext4 1MiB 100%
+    
+    echo "Creating ext4 partition..."
+    local PARTITION=$(lsblk "$DEVICE" --json --output name,path | jq -r '.blockdevices[] .children[]? .path')
+    mkfs.ext4 -F "$PARTITION"
+    
+    echo "Updating partition label to $NODE_LABEL..."
+    e2label "$PARTITION" "$NODE_LABEL"
+  fi
+}
 
